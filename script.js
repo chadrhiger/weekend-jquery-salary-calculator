9f8100c446d5b8696d1fa91baabf136c352b0cff
Chad Rhiger <csrhig@gmail.com>
2022-04-09 15:27:15 -0500

    Got inputs to return values on click of submit button.

--- script.js
+++ script.js
@@ -3,15 +3,54 @@ console.log('JS sauce');
 $(document).ready(onReady);
 console.log('jq ready');
 
+
 function onReady(){
   console.log('jq onReady');
-  $('#submitButton').on('click');
-}
+  $('#submitButton').on('click', onSubmit);
+} // end onReady
 
-
+// set up click listener
+// log when button is responsive
 function onSubmit(){
-  
-}
+  // console.log('click');  
+  let firstNameInput = $('#firstNameInput').val();
+  let lastNameInput = $('#lastNameInput').val();
+  let employeeIDInput = $('#employeeIDInput').val();
+  let titleInput = $('#titleInput').val();
+  let annualSalaryInput = $('#annualSalaryInput').val();
+  console.log(firstNameInput);
+  console.log(lastNameInput);
+  console.log(employeeIDInput);
+  console.log(titleInput);
+  console.log(annualSalaryInput);
+
+
+} // end onSubmit
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
