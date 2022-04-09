9b2ac67482c769d0319ad07230e535ecd420e317
Chad Rhiger <csrhig@gmail.com>
2022-04-09 14:01:00 -0500

    Sourced jq and js into html. Set up table in html and css to basic function, ready to start wiring up the script. Brought jq into js.

---
+++ script.js
@@ -0,0 +1,23 @@
+console.log('JS sauce');
+
+$(document).ready(onReady);
+console.log('jq ready');
+
+function onReady(){
+  console.log('jq onReady');
+  $('#submitButton').on('click');
+}
+
+
+function onSubmit(){
+  
+}
+
+
+
+
+
+
+
+
+// let input = document.createElement('input');   ????
